a38ca43fc3ce92df968f627c239a6eea87ae49a7
michiel.hendriks <michiel.hendriks@mp-objects.com>
2018-06-09 16:41:56 +0200

    moved travis maven settings to settings.xml

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -Dtravis-ossrh=true -DskipTests=true
+    mvn deploy --settings $GPG_DIR/settings.xml -Ptravis-ossrh -DskipTests=true
     exit $?
 fi
